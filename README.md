d3a439f0442e52a96c015a6844d892d544543a9e
SHINU4RATHOD <shinukrathod0143@gmail.com>
2024-12-25 13:48:48 +0530

    altered user input function

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 
 ### STEP 01- Create a conda environment after opening the repository
 ```bash
-conda create -n medibot python=3.10 -y
+conda create -n multpdfchat python=3.10 -y
 ```
 
 ```bash
-conda activate medibot
+conda activate multpdfchat
 ```
 
 ### STEP 02- install the requirements
--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
 from langchain.vectorstores import FAISS
+import google.generativeai as genai
+from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-
-
-
 
 
 def get_pdf_text(pdf_docs):
@@ -52,29 +48,19 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
-
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
     return chain
 
 
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
-
     chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
+    response = chain({"input_documents":docs, "question": user_question}, return_only_outputs=True)
     print(response)
     st.write("Reply: ", response["output_text"])
 
@@ -83,7 +69,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with All Your PDF using Gemini💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -92,14 +78,13 @@ def main():
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDF's Files and Hit on the Submit & Process Button", accept_multiple_files=True)
         if st.button("Submit & Process"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
-
 
 
 if __name__ == "__main__":
